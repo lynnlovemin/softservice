980853fde368b61c73583c8204dd99d96ef36463
liyi <331509758@qq.com>
2018-01-08 13:28:08 +0800

    修改了数据库表名

--- user/src/main/java/com/lynn/user/mapper/ApplicationMapper.java
+++ user/src/main/java/com/lynn/user/mapper/ApplicationMapper.java
@@ -8,9 +8,9 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface ApplicationMapper {
 
-    @Select("select client_id,secret from application where client_id = #{clientId}")
+    @Select("select client_id,secret from oauth_application where client_id = #{clientId}")
     ApplicationOut findByClientId(@Param("clientId") String clientId);
 
-    @Select("select client_id,secret from application where client_id = #{clientId} and secret = #{secret}")
+    @Select("select client_id,secret from oauth_application where client_id = #{clientId} and secret = #{secret}")
     ApplicationOut findByClientIdAndSecret(@Param("clientId") String clientID,@Param("secret") String secret);
 }
