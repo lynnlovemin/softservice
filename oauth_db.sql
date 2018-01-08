223c2d2d75a87675ca5415a5a54140760f089ecc
liyi <331509758@qq.com>
2018-01-08 13:29:30 +0800

    新增了mysql脚本文件

---
+++ oauth_db.sql
@@ -0,0 +1,53 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50719
+ Source Host           : localhost
+ Source Database       : oauth_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 50719
+ File Encoding         : utf-8
+
+ Date: 01/08/2018 13:28:37 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `oauth_application`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_application`;
+CREATE TABLE `oauth_application` (
+  `id` bigint(16) NOT NULL AUTO_INCREMENT,
+  `client_id` varchar(32) NOT NULL,
+  `secret` varchar(32) NOT NULL,
+  `gmt_create` datetime DEFAULT CURRENT_TIMESTAMP,
+  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_client_id` (`client_id`),
+  KEY `uk_client_id_secret` (`client_id`,`secret`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+--  Table structure for `oauth_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_user`;
+CREATE TABLE `oauth_user` (
+  `id` bigint(16) NOT NULL AUTO_INCREMENT,
+  `mobile` varchar(16) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nickname` varchar(32) NOT NULL,
+  `openid` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_openid` (`openid`),
+  UNIQUE KEY `uk_mobile` (`mobile`) USING BTREE,
+  KEY `uk_mobile_password` (`mobile`,`password`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+SET FOREIGN_KEY_CHECKS = 1;
